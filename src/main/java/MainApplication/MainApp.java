f5856f878bd10d4ecc2b3fff66bbae8e40672b5f
Wildboyee <wildboyee@Wildboyees-MBP.fios-router.home>
2019-04-17 20:59:17 -0400

    Second commit

---
+++ src/main/java/MainApplication/MainApp.java
@@ -0,0 +1,19 @@
+package MainApplication;
+
+import abc.cd.spring1st.dto.Customer;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Customer c= new Customer();
+		c.setId(1);
+		c.setFullName("abishekgauchan");
+		c.setEmail("abishekgauchan@gmail.com");
+		c.setPhone("2404490889");
+		
+		
+
+	}
+
+}
--- src/main/java/abc/cd/spring1st/dto/customer.java
+++ src/main/java/abc/cd/spring1st/dto/customer.java
@@ -1,5 +1,41 @@
 package abc.cd.spring1st.dto;
 
-public class customer {
+public class Customer {
+	private int id;
+	private String fullName;
+	private String email;
+	private String phone;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
 
 }
